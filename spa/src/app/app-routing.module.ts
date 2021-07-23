71c5b754e661f5afeb876775abbe84a2a58863ce
Venugopal  K (C) <vgk@tritechsamvit.com>
2021-07-23 11:32:01 +0530

    Child Route Updated

--- spa/src/app/app-routing.module.ts
+++ spa/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './accounts/login/login.component';
+import { SignupComponent } from './accounts/signup/signup.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   /*Route instance - Map Component ->Path */
-{path:'home',component:HomeComponent},
+{path:'home',component:HomeComponent,children:[
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent}
+]},
 {path:'', redirectTo:'/home', pathMatch:'full'}
 
 ];
